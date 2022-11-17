e8c733d477c50840a353125383a3e17a2af8a118
Luís Gustavo <luisgsilva270@gmail.com>
2022-11-17 00:41:16 +0000

    update

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,21 +1,21 @@
 """
 Tests for models
 """
-#from django.test import TestCase
-#from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 
-# class ModelTests(TestCase):
-#     """Test models."""
+class ModelTests(TestCase):
+    """Test models."""
 
-#     def test_create_user_with_email_successful(self):
-#         """Test creating a user with an email is successful."""
-#         email = 'test@example.com'
-#         password = 'testpass123'
-#         user = get_user_model().objects.create_user(
-#             email=email,
-#             password=password,
-#         )
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
 
-#         self.assertEqual(user.email, email)
-#         self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
