00eac7683b08b055894b1bed5b968b2a944666c2
Luís Gustavo <luisgsilva270@gmail.com>
2022-11-20 19:24:19 +0000

    update

--- app/app/settings.py
+++ app/app/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
+print(f'Database HOST Config: {os.environ.get("DB_HOST")}')
+print(f'Database NAME Config: {os.environ.get("DB_NAME")}')
+print(f'Database USER Config: {os.environ.get("DB_USER")}')
+print(f'Database PASS Config: {os.environ.get("DB_PASS")}')
 
 
 # Password validation
