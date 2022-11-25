53f24b9e7d14e0184410e2cef627dbad5ad512bc
luisgs7 <luisgsilva270@gmail.com>
2022-11-25 16:24:25 -0300

    config admin fieldsets

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -11,6 +12,25 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering: list[str] = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
