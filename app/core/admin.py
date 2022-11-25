835b42b2d0f98d2fc002af98d0c24d164dfe9a2b
luisgs7 <luisgsilva270@gmail.com>
2022-11-25 11:49:15 -0300

    class UserAdmin ok

---
+++ app/core/admin.py
@@ -0,0 +1,16 @@
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering: list[str] = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,18 +17,18 @@ class AdminSiteTests(TestCase):
             email='admin@example.com',
             password='testpass123',
         )
-        
+
         self.client.force_login(self.admin_user)
-        
+
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
             name='Test User'
         )
-    
+
     def test_users_lists(self):
         """Test that usres ate listed on page."""
-        url = reverse('admin:core_user_clangelist')
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.email)
