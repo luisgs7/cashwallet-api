9ba909605cc4afc7efdf8d349ddd5c7f0fb96af6
Luís Gustavo <luisgsilva270@gmail.com>
2022-11-26 18:22:54 +0000

    Installed and configured drf_spectacular

--- app/app/settings.py
+++ app/app/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #TODO: My Apps
     'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
