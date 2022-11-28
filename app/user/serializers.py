bd3b5bc9684498b5e4151d8170ff5be880a94fd5
Luís Gustavo <luisgsilva270@gmail.com>
2022-11-28 20:19:23 +0000

    add min_length config

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -10,10 +10,11 @@ class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
 
     class Meta:
+        """Class default."""
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {'write_only': True}}
-    
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
     def create(self, validated_data):
         """Create and return a new user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
